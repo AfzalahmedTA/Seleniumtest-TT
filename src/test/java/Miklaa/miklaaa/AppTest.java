55ac327e84f7aeccb07d6309cdb92348e5cc160d
AfzalahmedTA <afsalahmd4@gmail.com>
2022-12-27 11:22:37 +0530

    Second commit

---
+++ src/test/java/Miklaa/miklaaa/AppTest.java
@@ -0,0 +1,13 @@
+package Miklaa.miklaaa;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
